bce547a0c664fef048f7e05c47f50e2a7d3a8b42
lil_rob_33 <48527902+lilrob33@users.noreply.github.com>
2020-09-21 12:47:33 -0700

    Delete axios.js

--- src/src/axios.js
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const instance = axios.create({
-  // THE API (cloud function) URL
-  baseURL: "https://us-central1-challenge-5a7a6.cloudfunctions.net/api",
-  // "http://localhost:5001/challenge-5a7a6/us-central1/api"
-});
-
-export default instance;
